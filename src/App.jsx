a25815f6825df491b2737c04365890f5dd7f413d
sreedev777 <sreedevkailas@gmail.com>
2026-07-11 10:20:14 +0530

    search commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import StudentEntry from './components/StudentEntry'
+import SearchStudent from './components/SearchStudent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
         <StudentEntry/>
+        <SearchStudent/>
     </>
   )
 }
